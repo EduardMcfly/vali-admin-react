fc1a6b26b83024afdbec33b2f98d2919641195db
joyeria2018 <sipsajoyeria2018@gmail.com>
2018-08-29 16:56:21 -0500

    add home and i18next

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 // Styles
 // CoreUI Icons Set
@@ -11,29 +11,55 @@ import 'font-awesome/css/font-awesome.min.css';
 // Import Simple Line Icons Set
 import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
-import './scss/style.css'
+/* import './scss/style.css'; */
+import '../sass/app.css';
 
 // Containers
 import { DefaultLayout } from './containers';
 // Pages
-import { Login, Page404, Page500, Register } from './views/Pages';
+import { Login, Page404, Page500, Register,Home } from './views/Pages';
 
 // import { renderRoutes } from 'react-router-config';
 
+const fakeAuth = {
+    isAuthenticated: true,
+    authenticate(cb) {
+        console.log(cb);
+        this.isAuthenticated = true;
+        setTimeout(cb, 100);
+    },
+    signout(cb) {
+        console.log(cb);
+        this.isAuthenticated = false;
+        setTimeout(cb, 100);
+    },
+};
+fakeAuth.signout();
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props => (fakeAuth.isAuthenticated === true ? <Component {...props} /> : <Redirect to="/login" />)}
+    />
+);
+
 class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <Switch>
-          <Route exact path="/login" name="Login Page" component={Login} />
-          <Route exact path="/register" name="Register Page" component={Register} />
-          <Route exact path="/404" name="Page 404" component={Page404} />
-          <Route exact path="/500" name="Page 500" component={Page500} />
-          <Route path="/" name="Home" component={DefaultLayout} />
-        </Switch>
-      </HashRouter>
-    );
-  }
+    render() {
+        return (
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/login" name="Login Page" component={Login} />
+                    <Route exact path="/register" name="Register Page" component={Register} />
+                    <Route exact path="/404" name="Page 404" component={Page404} />
+                    <Route exact path="/404" name="Page 404" component={Page404} />
+                    <Route exact path="/500" name="Page 500" component={Page500} />
+                    <Route exact path="/home" name="Home" component={Home} />
+                    <PrivateRoute path="/" name="Home" component={DefaultLayout} />
+                </Switch>
+            </HashRouter>
+        );
+    }
 }
 
 export default App;
--- views/Pages/Login/Login.js
+++ views/Pages/Login/Login.js
@@ -1,63 +1,142 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import {
+    Button,
+    Card,
+    CardBody,
+    CardGroup,
+    Col,
+    Container,
+    Form,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    Row,
+} from 'reactstrap';
+import { I18n, Trans } from 'react-i18next';
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="app flex-row align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <CardGroup>
-                <Card className="p-4">
-                  <CardBody>
-                    <Form>
-                      <h1>Login</h1>
-                      <p className="text-muted">Sign In to your account</p>
-                      <InputGroup className="mb-3">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-user"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="text" placeholder="Username" autoComplete="username" />
-                      </InputGroup>
-                      <InputGroup className="mb-4">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-lock"></i>
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input type="password" placeholder="Password" autoComplete="current-password" />
-                      </InputGroup>
-                      <Row>
-                        <Col xs="6">
-                          <Button color="primary" className="px-4">Login</Button>
-                        </Col>
-                        <Col xs="6" className="text-right">
-                          <Button color="link" className="px-0">Forgot password?</Button>
-                        </Col>
-                      </Row>
-                    </Form>
-                  </CardBody>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Button color="primary" className="mt-3" active>Register Now!</Button>
-                    </div>
-                  </CardBody>
-                </Card>
-              </CardGroup>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app flex-row align-items-center">
+                <I18n ns="header">
+                    {(t, { i18n }) => (
+                        <header className="app-header pr-1">
+                            <a className="app-header__logo" href="#home">
+                                Cosva
+                            </a>
+                            <ul className="app-nav">
+                                <li className="dropdown">
+                                    <a
+                                        className="app-nav__item"
+                                        href="#"
+                                        data-toggle="dropdown"
+                                        aria-label="Open Profile Menu"
+                                    >
+                                        <i className="fa fa-ellipsis-v fa-2x" />
+                                    </a>
+                                    <ul className="dropdown-menu settings-menu dropdown-menu-right">
+                                        <li>
+                                            <button className="dropdown-item" onClick={() => i18n.changeLanguage('es')}>
+                                                <i className="fa fa-language fa-lg" />
+                                                Español
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button className="dropdown-item" onClick={() => i18n.changeLanguage('en')}>
+                                                <i className="fa fa-language fa-lg" />
+                                                English
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <a className="dropdown-item" href="{{ route('logout') }}">
+                                                <i className="fa fa-sign-out fa-lg" />
+                                                {t('exit')}
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </header>
+                    )}
+                </I18n>
+
+                <Container>
+                    <I18n ns="login">
+                        {(t, { i18n }) => (
+                            <Row className="justify-content-center">
+                                <Col md="12" lg="10">
+                                    <CardGroup>
+                                        <Card className="p-4">
+                                            <CardBody>
+                                                <div>
+                                                    <h1>{t('loginTitle')}</h1>
+                                                    <p className="text-muted">{t('loginSubtitle')}</p>
+                                                    <InputGroup className="mb-3">
+                                                        <InputGroupAddon addonType="prepend">
+                                                            <InputGroupText>
+                                                                <i className="icon-user" />
+                                                            </InputGroupText>
+                                                        </InputGroupAddon>
+                                                        <Input
+                                                            type="text"
+                                                            placeholder={t('emailTitle')}
+                                                            autoComplete="username"
+                                                        />
+                                                    </InputGroup>
+                                                    <InputGroup className="mb-4">
+                                                        <InputGroupAddon addonType="prepend">
+                                                            <InputGroupText>
+                                                                <i className="icon-lock" />
+                                                            </InputGroupText>
+                                                        </InputGroupAddon>
+                                                        <Input
+                                                            type="password"
+                                                            placeholder={t('password')}
+                                                            autoComplete="current-password"
+                                                        />
+                                                    </InputGroup>
+                                                    <Row>
+                                                        <Col sm="12" lg="6">
+                                                            <Button color="primary" className="w-100 px-4 text-truncate">
+                                                                {t('loginTitle')}
+                                                            </Button>
+                                                        </Col>
+                                                        <Col sm="12" lg="6" className="text-right">
+                                                            <Button color="link" className="text-truncate w-100 px-0">
+                                                                {t('forgotPassword')}
+                                                            </Button>
+                                                        </Col>
+                                                    </Row>
+                                                </div>
+                                            </CardBody>
+                                        </Card>
+                                        <Card
+                                            className="d-none d-md-block text-white bg-primary py-5 d-md-down-none"
+                                            style={{ width: 44 + '%' }}
+                                        >
+                                            <CardBody className="text-center">
+                                                <div>
+                                                    <h2>Sign up</h2>
+                                                    <p>
+                                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                                                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                    </p>
+                                                    <Button color="primary" className="mt-3" active>
+                                                        Register Now!
+                                                    </Button>
+                                                </div>
+                                            </CardBody>
+                                        </Card>
+                                    </CardGroup>
+                                </Col>
+                            </Row>
+                        )}
+                    </I18n>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default Login;
